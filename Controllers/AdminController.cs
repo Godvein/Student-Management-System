cd10a817f6b45ab5899796f3501fc19ac8047ba5
ankit <ankitdware101@gmail.com>
2026-05-03 02:19:19 -0700

    feat: added Admin features
    - added AdminController endpoints can assign or remove roles if
      authorized

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using StudentMS.Data;
+namespace StudentMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public AdminController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("assign-role")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> AssignRoles(int UserId, int RoleId)
+        {
+            var user = await _context.Users.FindAsync(UserId);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var role = await _context.Roles.FindAsync(RoleId);
+            if (role == null)
+            {
+                return NotFound("Role not found");
+            }
+            user.Roles.Add(role);
+            await _context.SaveChangesAsync();
+            return Ok("Role assigned successfully");
+        }
+
+        [HttpPost("remove-role")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> RemoveRoles(int UserId, int RoleId)
+        {
+            var user = await _context.Users.Include(u => u.Roles).FirstOrDefaultAsync(u => u.UserId == UserId);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var role = await _context.Roles.FindAsync(RoleId);
+            if (role == null)
+            {
+                return NotFound("Role not found");
+            }
+            user.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+            return Ok("Role removed successfully");
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
